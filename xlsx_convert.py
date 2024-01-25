5ab12d5caa2994e60e692522ecbaf5e6ee11aaf8
yoseflaw <yosefardhitowin@gmail.com>
2024-01-25 08:16:29 +0700

    add read and write blob

--- xlsx_convert.py
+++ xlsx_convert.py
@@ -18,6 +18,13 @@ def convert_xlsx_to_csv(
         out_file_path = os.path.join(out_folder, filename)
         print(blob_path, filename, out_file_path)
 
+        bucket = storage_client.bucket(bucket_name)
+        out_blob = bucket.blob(out_file_path)
+
+        with blob.open("rb") as fin, out_blob.open("wb") as fout:
+            df = pd.read_excel(fin)
+            df.to_csv(fout, index=False)
+
 if __name__=="__main__":
     convert_xlsx_to_csv(
         "telmark-gcs-development", 
